7cefb19cdf5da50226d7441a5fa742f6a15fd115
yannick.sanajong <yannick.sanajong@global.ntt>
2022-07-05 21:35:29 +0200

    updates

--- test/api/dhl-api.js
+++ test/api/dhl-api.js
@@ -30,8 +30,9 @@ function getDHL() {
     }
 }
 
-function rR() {
+function rW() {
         document.getElementById('wrapper').style.backgroundColor = 'white';
+        document.getElementById('error').style.display = 'none';
     }
 
 async function geT() {
@@ -40,31 +41,34 @@ async function geT() {
     const parameters = new URLSearchParams({
         trackingNumber: inPut,
     });
-    const response = await fetch(`https://api-eu.dhl.com/track/shipments?${parameters.toString}`, {
+    const response = await fetch(`https://api-eu.dhl.com/track/shipments?${ parameters.toString() }`, {
         method: 'GET',
         headers: {
             'DHL-API-Key': 'pO2qP8ianK1QwSAnC2zgIyN3rxR14bzn',
         },
     } );
-    if (response !== 200) {
+    if (response.status !== 200) {
         document.getElementById('error').style.display = 'block';
         document.getElementById('wrapper').style.backgroundColor = 'red';
+        console.log("Shit..")
     } else {
         const d = await response.json();
+        console.log(`OK gelukt, laat me ff alles doornemen`)
+        console.log(d)
         document.getElementById('ref-num').textContent = d.shipments[0].details.references[0].number
         document.getElementById('o-country').textContent = d.shipments[0].origin.address.countryCode
         document.getElementById('o-address').textContent = d.shipments[0].origin.address.addressLocality
         document.getElementById('d-address').textContent = d.shipments[0].destination.address.addressLocality
         document.getElementById('d-country').textContent = d.shipments[0].destination.address.countryCode
-        document.getElementById('proofOfDelivery').textContent = d.shipments[0].details.proofOfDelivery.timestamp
-        document.getElementById('proofOfDeliverySignedAvailable').textContent = d.shipments[0].details.proofOfDeliverySignedAvailable
-        document.getElementById('totalNumberOfPieces').textContent = d.shipments[0].details.totalNumberOfPieces
-        document.getElementById('weight-ut').textContent = d.shipments[0].details.weight.unitText
-        document.getElementById('weight-v').textContent = d.shipments[0].details.weight.value
-        document.getElementById('volume-ut').textContent = d.shipments[0].details.volume.unitText
-        document.getElementById('volume-v').textContent = d.shipments[0].details.volume.value
-        document.getElementById('').textContent = d.shipments[0].events[0] // need to iterate trough as number of events might defer..
-        document.getElementById('').textContent = d.shipments[0].events.length // to see the number of events in the object.
+        // document.getElementById('proofOfDelivery').textContent = d.shipments[0].details.proofOfDelivery.timestamp
+        // document.getElementById('proofOfDeliverySignedAvailable').textContent = d.shipments[0].details.proofOfDeliverySignedAvailable
+        // document.getElementById('totalNumberOfPieces').textContent = d.shipments[0].details.totalNumberOfPieces
+        // document.getElementById('weight-ut').textContent = d.shipments[0].details.weight.unitText
+        // document.getElementById('weight-v').textContent = d.shipments[0].details.weight.value
+        // document.getElementById('volume-ut').textContent = d.shipments[0].details.volume.unitText
+        // document.getElementById('volume-v').textContent = d.shipments[0].details.volume.value
+        // document.getElementById('').textContent = d.shipments[0].events[0] // need to iterate trough as number of events might defer..
+        // document.getElementById('').textContent = d.shipments[0].events.length // to see the number of events in the object.
         document.getElementById('status-desc').textContent = d.shipments[0].status.description
         document.getElementById('status-code').textContent = d.shipments[0].status.statusCode
         document.getElementById('status-time').textContent = d.shipments[0].status.timestamp
