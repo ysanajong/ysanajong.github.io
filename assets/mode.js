4963bc1c9ce99639efea8f3e8a5f594335ca0707
Yannick San-A-Jong <yannick@sanajong.net>
2022-07-11 14:04:41 +0200

    sync (#17)
    * moved toggle css to seperate scss file
    
    * update
    
    * o
    
    * .
    
    * update
    
    * removed unused files
    
    * test
    
    * removed old button html
    
    * modified button properties
    
    * removed outline
    
    * updated colors, removed duplicates
    
    * small fix
    
    * 3 add dark light theme (#16)
    
    * changed button color
    
    * Cosmetic changes
    
    * R-Moved unused file
    
    * added background on hover
    
    Co-authored-by: yannick.sanajong <yannick.sanajong@global.ntt>
    
    Co-authored-by: yannick.sanajong <yannick.sanajong@global.ntt>

--- assets/mode.js
+++ assets/mode.js
@@ -1,6 +1,6 @@
 // check for saved 'darkMode' in localStorage
 let darkMode = localStorage.getItem('darkMode'); 
-const darkModeToggle = document.getElementById('theme-toggle');
+let darkModeToggle = document.getElementById('theme-toggle');
 
 const enableDarkMode = () => {
   // 1. Add the class to the body
@@ -9,6 +9,7 @@ const enableDarkMode = () => {
   // 2. Update darkMode in localStorage
   localStorage.setItem('darkMode', 'enabled');
   darkModeToggle.checked = true
+  return
 }
 const disableDarkMode = () => {
   // 1. Remove the class from the body
@@ -18,21 +19,17 @@ const disableDarkMode = () => {
   localStorage.setItem('darkMode', null);
   document.getElementById('theme-toggle').checked = false
 }
-window.matchMedia('(prefers-color-scheme: dark)')
-      .addEventListener('change', (event) => {
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (event) => {
        event.matches ? localStorage.setItem('darkMode', 'enabled') : localStorage.setItem('darkMode', 'disabled');
       });
 
-/* window.matchMedia('(prefers-color-scheme: light)')
-      .addEventListener('change', (event) => {
+window.matchMedia('(prefers-color-scheme: light)').addEventListener('change', (event) => {
         event.matches ? disableDarkMode() : enableDarkMode();
       });
-  */
+ 
 // If the user already visited and enabled darkMode
 // start things off with it on
-if (darkMode === 'enabled') {
-  enableDarkMode();
-}
+if (darkMode === 'enabled') { enableDarkMode(); }
 // When button is checked
 darkModeToggle.addEventListener('change', () => {
   darkModeToggle.checked ? enableDarkMode() : disableDarkMode();
